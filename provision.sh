51d8098c98201c447419dc6a392867eb72f8c5a7
Jon Cram <webignition@gmail.com>
2021-07-07 14:37:40 +0100

    Setup messenger transports before updating schema

--- provision.sh
+++ provision.sh
@@ -39,4 +39,5 @@ sudo \
   WORKER_IMAGE="$WORKER_IMAGE" \
   docker-compose up -d
 sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
+sudo docker-compose exec -T app php bin/console messenger:setup-transports
 sudo docker-compose exec -T app php bin/console doctrine:schema:update --force
