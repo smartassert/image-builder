ab43a670c412245423445224b7e95996df4cde8a
Jon Cram <webignition@gmail.com>
2021-07-07 11:07:18 +0100

    Rename digitalocean_image to worker_image

--- provision.sh
+++ provision.sh
@@ -36,7 +36,7 @@ chown -R www-data:www-data /var/log
 sudo \
   WORKER_MANAGER_VERSION="$WORKER_MANAGER_VERSION" \
   DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN" \
-  DIGITALOCEAN_IMAGE="$DIGITALOCEAN_IMAGE" \
+  WORKER_IMAGE="$WORKER_IMAGE" \
   docker-compose up -d
 sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
 sudo docker-compose exec -T app php bin/console doctrine:schema:update --force
