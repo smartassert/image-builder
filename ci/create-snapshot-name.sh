f426d3623c535cdf4377dd6313d738e0b4d1f3b5
Jon Cram <webignition@gmail.com>
2021-07-09 19:00:40 +0100

    Simplify release workflow (#19)

--- ci/create-snapshot-name.sh
+++ ci/create-snapshot-name.sh
@@ -12,13 +12,11 @@ EXIT_CODE_INVALID_EVENT=1
 # EVENT_NAME='release' RELEASE_TAG_NAME='0.1' ./create-snapshot-name.sh
 
 if [ $EVENT_NAME = "push" ]; then
-  SUFFIX="master"
+  echo "master"
 elif [ $EVENT_NAME = "pull_request" ]; then
-  SUFFIX="pull-request-${PR_NUMBER}"
+  echo "pull-request-${PR_NUMBER}"
 elif [ $EVENT_NAME = "release" ]; then
-  SUFFIX="release-$(echo $RELEASE_TAG_NAME | tr -d '"')"
+  echo "release-$(echo $RELEASE_TAG_NAME | tr -d '"')"
 else
   exit $EXIT_CODE_INVALID_EVENT
 fi
-
-echo "::set-output name=value::${SUFFIX}"
--- ci/create-version-label.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z "$RELEASE_TAG_NAME" ]]; then
-  echo "master"
-  exit 0
-fi
-
-echo "$RELEASE_TAG_NAME"
-exit 0
