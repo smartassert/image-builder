0934ac24c1134104a9ac52e7079f6f7f6799f528
Jon Cram <webignition@gmail.com>
2021-07-21 14:20:44 +0100

    Add InstanceCollectionHydrator::hydrate()

--- app/src/Services/InstanceCollectionHydrator.php
+++ app/src/Services/InstanceCollectionHydrator.php
@@ -25,4 +25,19 @@ class InstanceCollectionHydrator
 
         return new InstanceCollection($hydratedInstances);
     }
+
+    public function hydrate(InstanceCollection $collection): InstanceCollection
+    {
+        $hydratedInstances = [];
+
+        foreach ($collection as $instance) {
+            try {
+                $hydratedInstances[] = $this->instanceHydrator->hydrate($instance);
+            } catch (ClientExceptionInterface) {
+                // Intentionally ignore HTTP exceptions
+            }
+        }
+
+        return new InstanceCollection($hydratedInstances);
+    }
 }
--- app/tests/Functional/Services/InstanceCollectionHydratorTest.php
+++ app/tests/Functional/Services/InstanceCollectionHydratorTest.php
@@ -65,6 +65,45 @@ class InstanceCollectionHydratorTest extends KernelTestCase
         }
     }
 
+    public function testHydrate(): void
+    {
+        $instanceCollectionData = [
+            123 => [
+                'ipAddress' => '127.0.0.1',
+                'version' => '0.1',
+                'message-queue-size' => 14,
+            ],
+            456 => [
+                'ipAddress' => '127.0.0.2',
+                'version' => '0.2',
+                'message-queue-size' => 7,
+            ],
+        ];
+
+        $instances = [];
+        foreach ($instanceCollectionData as $dropletId => $instanceData) {
+            $instances[] = new Instance($this->createDroplet($dropletId, $instanceData['ipAddress']));
+            $this->mockHandler->append($this->httpResponseFactory->createFromArray([
+                HttpResponseFactory::KEY_STATUS_CODE => 200,
+                HttpResponseFactory::KEY_BODY => json_encode([
+                    'version' => $instanceData['version'],
+                    'message-queue-size' => $instanceData['message-queue-size'],
+                ]),
+            ]));
+        }
+
+        $instanceCollection = new InstanceCollection($instances);
+        $hydratedCollection = $this->instanceCollectionHydrator->hydrate($instanceCollection);
+
+        foreach ($hydratedCollection as $hydratedInstance) {
+            $expectedData = $instanceCollectionData[$hydratedInstance->getId()];
+            $expectedVersion = $expectedData['version'];
+            $expectedMessageQueueSize = $expectedData['message-queue-size'];
+            self::assertSame($expectedVersion, $hydratedInstance->getVersion());
+            self::assertSame($expectedMessageQueueSize, $hydratedInstance->getMessageQueueSize());
+        }
+    }
+
     private function createDroplet(int $id, string $ipAddress): DropletEntity
     {
         return new DropletEntity([
