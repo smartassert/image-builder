24ab6eb2b74e94dbc1be8015f89a86646f2fdecb
Jon Cram <webignition@gmail.com>
2021-07-23 11:24:00 +0100

    Add InstanceRepository::find()

--- app/tests/Functional/Services/InstanceRepositoryTest.php
+++ app/tests/Functional/Services/InstanceRepositoryTest.php
@@ -105,4 +105,48 @@ class InstanceRepositoryTest extends KernelTestCase
             ],
         ];
     }
+
+    /**
+     * @dataProvider findDataProvider
+     */
+    public function testFind(string $httpResponseBody, ?Instance $expectedInstance): void
+    {
+        $this->mockHandler->append(
+            $this->httpResponseFactory->createFromArray([
+                HttpResponseFactory::KEY_STATUS_CODE => 200,
+                HttpResponseFactory::KEY_HEADERS => [
+                    'content-type' => 'application/json; charset=utf-8',
+                ],
+                HttpResponseFactory::KEY_BODY => $httpResponseBody,
+            ])
+        );
+
+        $instance = $this->instanceRepository->find();
+        self::assertEquals($expectedInstance, $instance);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function findDataProvider(): array
+    {
+        return [
+            'not found' => [
+                'httpResponseBody' => (string) json_encode([
+                    'droplets' => [],
+                ]),
+                'expectedInstance' => null,
+            ],
+            'found' => [
+                'httpResponseBody' => (string) json_encode([
+                    'droplets' => [
+                        [
+                            'id' => 123,
+                        ],
+                    ],
+                ]),
+                'expectedInstance' => InstanceFactory::create(['id' => 123]),
+            ],
+        ];
+    }
 }
