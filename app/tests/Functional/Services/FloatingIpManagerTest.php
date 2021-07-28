1912c4f2a0ff029033ccb8daae72cd47c5f73cbd
Jon Cram <webignition@gmail.com>
2021-07-28 17:02:24 +0100

    Remove FloatingIpManager::assign() (#152)

--- app/tests/Functional/Services/FloatingIpManagerTest.php
+++ app/tests/Functional/Services/FloatingIpManagerTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\Functional\Services;
 
-use App\Model\FloatingIpAssignmentAction;
 use App\Model\Instance;
 use App\Services\FloatingIpManager;
 use App\Tests\Services\HttpResponseFactory;
@@ -33,113 +32,6 @@ class FloatingIpManagerTest extends KernelTestCase
         $httpResponseFactory = self::getContainer()->get(HttpResponseFactory::class);
         \assert($httpResponseFactory instanceof HttpResponseFactory);
         $this->httpResponseFactory = $httpResponseFactory;
-    }
-
-    /**
-     * @dataProvider assignDataProvider
-     *
-     * @param array<mixed> $httpResponseDataCollection
-     */
-    public function testAssign(
-        array $httpResponseDataCollection,
-        Instance $instance,
-        FloatingIpAssignmentAction $expectedAction
-    ): void {
-        foreach ($httpResponseDataCollection as $httpResponseData) {
-            $this->mockHandler->append(
-                $this->httpResponseFactory->createFromArray($httpResponseData)
-            );
-        }
-
-        $action = $this->floatingIpManager->assign($instance);
-
-        self::assertEquals($expectedAction, $action);
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function assignDataProvider(): array
-    {
-        $instance123 = InstanceFactory::create(['id' => 123]);
-        $instance456 = InstanceFactory::create(['id' => 456]);
-
-        return [
-            'no existing floating IP' => [
-                'httpResponseDataCollection' => [
-                    'repository response' => [
-                        HttpResponseFactory::KEY_STATUS_CODE => 200,
-                        HttpResponseFactory::KEY_HEADERS => [
-                            'content-type' => 'application/json; charset=utf-8',
-                        ],
-                        HttpResponseFactory::KEY_BODY => (string) json_encode([
-                            'floating_ips' => [],
-                        ]),
-                    ],
-                    'assign to droplet response' => [
-                        HttpResponseFactory::KEY_STATUS_CODE => 202,
-                        HttpResponseFactory::KEY_HEADERS => [
-                            'content-type' => 'application/json; charset=utf-8',
-                        ],
-                        HttpResponseFactory::KEY_BODY => (string) json_encode([
-                            'floating_ip' => [
-                                'ip' => '127.0.0.100',
-                            ],
-                        ]),
-                    ],
-                ],
-                'instance' => $instance123,
-                'expectedAction' => new FloatingIpAssignmentAction(
-                    '127.0.0.100',
-                    $instance123
-                ),
-            ],
-            'has existing floating IP' => [
-                'httpResponseDataCollection' => [
-                    'repository response' => [
-                        HttpResponseFactory::KEY_STATUS_CODE => 200,
-                        HttpResponseFactory::KEY_HEADERS => [
-                            'content-type' => 'application/json; charset=utf-8',
-                        ],
-                        HttpResponseFactory::KEY_BODY => (string) json_encode([
-                            'floating_ips' => [
-                                [
-                                    'ip' => '127.0.0.200',
-                                    'droplet' => [
-                                        'id' => 123,
-                                        'tags' => [
-                                            'worker-manager',
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ]),
-                    ],
-                    'assign to droplet response' => [
-                        HttpResponseFactory::KEY_STATUS_CODE => 202,
-                        HttpResponseFactory::KEY_HEADERS => [
-                            'content-type' => 'application/json; charset=utf-8',
-                        ],
-                        HttpResponseFactory::KEY_BODY => (string) json_encode([
-                            'action' => [
-                                'id' => 001,
-                                'type' => 'assign_ip',
-                            ],
-                        ]),
-                    ],
-                ],
-                'instance' => $instance456,
-                'expectedAction' => (new FloatingIpAssignmentAction(
-                    '127.0.0.200',
-                    $instance456
-                ))->withActionEntity(
-                    new ActionEntity([
-                        'id' => 001,
-                        'type' => 'assign_ip',
-                    ])
-                ),
-            ],
-        ];
     }
 
     /**
