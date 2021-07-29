f76d1a146f8149417f2a8fbfe20305d0043f114a
Jon Cram <webignition@gmail.com>
2021-07-29 12:21:40 +0100

    Create IpGetCommand

---
+++ app/src/Command/IpGetCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Command;
+
+use App\Model\AssignedIp;
+use App\Services\FloatingIpRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: IpGetCommand::NAME,
+    description: 'Get the current floating IP. Output IP if present',
+)]
+class IpGetCommand extends Command
+{
+    public const NAME = 'app:ip:get';
+
+    public function __construct(
+        private FloatingIpRepository $floatingIpRepository,
+        string $name = null
+    ) {
+        parent::__construct($name);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $assignedIp = $this->floatingIpRepository->find();
+        if ($assignedIp instanceof AssignedIp) {
+            $output->write($assignedIp->getIp());
+        }
+
+        return Command::SUCCESS;
+    }
+}
