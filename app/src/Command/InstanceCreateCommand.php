ec55b7077dfa86085b165312a881c0aafcce6f36
Jon Cram <webignition@gmail.com>
2021-08-09 12:17:56 +0100

    Remove use of CommandExceptionRenderer in InstanceCreateCommand

--- app/src/Command/InstanceCreateCommand.php
+++ app/src/Command/InstanceCreateCommand.php
@@ -2,14 +2,12 @@
 
 namespace App\Command;
 
-use App\Services\CommandExceptionRenderer;
 use App\Services\InstanceRepository;
 use DigitalOceanV2\Exception\ExceptionInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: InstanceCreateCommand::NAME,
@@ -21,7 +19,6 @@ class InstanceCreateCommand extends Command
 
     public function __construct(
         private InstanceRepository $instanceRepository,
-        private CommandExceptionRenderer $commandExceptionRenderer,
     ) {
         parent::__construct(null);
     }
@@ -31,16 +28,9 @@ class InstanceCreateCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        try {
-            $instance = $this->instanceRepository->findCurrent();
-            if (null === $instance) {
-                $instance = $this->instanceRepository->create();
-            }
-        } catch (ExceptionInterface $e) {
-            $io = new SymfonyStyle($input, $output);
-            $io->error($this->commandExceptionRenderer->render($e));
-
-            throw $e;
+        $instance = $this->instanceRepository->findCurrent();
+        if (null === $instance) {
+            $instance = $this->instanceRepository->create();
         }
 
         $output->write((string) $instance->getId());
