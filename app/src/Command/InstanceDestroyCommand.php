997443b3be145e889b8d633b564ebbf8458de7fc
Jon Cram <webignition@gmail.com>
2021-08-11 10:36:28 +0100

    Create base command for single instance actions (#242)

--- app/src/Command/InstanceDestroyCommand.php
+++ app/src/Command/InstanceDestroyCommand.php
@@ -2,56 +2,28 @@
 
 namespace App\Command;
 
-use App\Services\InstanceRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: self::NAME,
     description: 'Destroy an instance.',
 )]
-class InstanceDestroyCommand extends Command
+class InstanceDestroyCommand extends AbstractInstanceActionCommand
 {
     public const NAME = 'app:instance:destroy';
 
-    private const OPTION_ID = 'id';
-
-    public function __construct(
-        private InstanceRepository $instanceRepository,
-        string $name = null
-    ) {
-        parent::__construct($name);
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption(
-                self::OPTION_ID,
-                null,
-                InputOption::VALUE_REQUIRED,
-                'ID of the instance to destroy'
-            )
-        ;
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $id = $this->getIdFromInput($input);
+        parent::execute($input, $output);
+
+        $id = $this->getId();
         if (is_int($id)) {
             $this->instanceRepository->delete($id);
         }
 
         return Command::SUCCESS;
-    }
-
-    private function getIdFromInput(InputInterface $input): ?int
-    {
-        $id = $input->getOption(self::OPTION_ID);
-
-        return ctype_digit($id) ? (int) $id : null;
     }
 }
--- app/src/Command/InstanceIsHealthyCommand.php
+++ app/src/Command/InstanceIsHealthyCommand.php
@@ -10,34 +10,24 @@ use Psr\Http\Client\ClientExceptionInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
     name: InstanceIsHealthyCommand::NAME,
     description: 'Perform instance health check',
 )]
-class InstanceIsHealthyCommand extends Command
+class InstanceIsHealthyCommand extends AbstractInstanceActionCommand
 {
     public const NAME = 'app:instance:is-healthy';
     public const EXIT_CODE_ID_INVALID = 3;
     public const EXIT_CODE_NOT_FOUND = 4;
 
-    private const OPTION_ID = 'id';
-
     public function __construct(
-        private InstanceRepository $instanceRepository,
+        InstanceRepository $instanceRepository,
         private InstanceClient $instanceClient,
         private CommandOutputHandler $outputHandler,
     ) {
-        parent::__construct(null);
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption(self::OPTION_ID, null, InputOption::VALUE_REQUIRED, 'Instance ID')
-        ;
+        parent::__construct($instanceRepository);
     }
 
     /**
@@ -46,17 +36,13 @@ class InstanceIsHealthyCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        parent::execute($input, $output);
+
         $this->outputHandler->setOutput($output);
 
-        $id = $this->getIdFromInput($input);
-
+        $id = $this->getId();
         if (null === $id) {
-            $presentationId = $input->getOption(self::OPTION_ID);
-            if (is_array($presentationId)) {
-                $presentationId = 'array: ' . implode(',', $presentationId);
-            }
-
-            $this->outputHandler->createErrorOutput('id-invalid', ['id' => $presentationId]);
+            $this->outputHandler->createErrorOutput('id-invalid');
 
             return self::EXIT_CODE_ID_INVALID;
         }
@@ -76,12 +62,5 @@ class InstanceIsHealthyCommand extends Command
         ]);
 
         return $health->isAvailable() ? Command::SUCCESS : Command::FAILURE;
-    }
-
-    private function getIdFromInput(InputInterface $input): ?int
-    {
-        $id = $input->getOption(self::OPTION_ID);
-
-        return ctype_digit($id) ? (int) $id : null;
     }
 }
