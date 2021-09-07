d80c561e01a2d4312b9aa0f98ccd8abaf7bb8b94
Jon Cram <webignition@gmail.com>
2021-09-07 14:18:49 +0100

    Uncomment

--- provision.sh
+++ provision.sh
@@ -31,22 +31,22 @@ run_command_until_successful sudo apt-get install -y \
 
 run_command_until_successful sudo apt autoremove -y
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-#
-#DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
-#if [ ! -f "$DOCKER_COMPOSE_BIN" ]; then
-#  sudo curl \
-#    -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
-#    -o /usr/local/bin/docker-compose
-#  sudo chmod +x /usr/local/bin/docker-compose
-#fi
-#
-#mkdir -p /var/log
-#chown -R www-data:www-data /var/log
-#
-#sudo \
-#  WORKER_MANAGER_VERSION="$WORKER_MANAGER_VERSION" \
-#  DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN" \
-#  docker-compose up -d
-#sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
-#sudo docker-compose exec -T app php bin/console messenger:setup-transports
-#sudo docker-compose exec -T app php bin/console doctrine:schema:update --force
+
+DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
+if [ ! -f "$DOCKER_COMPOSE_BIN" ]; then
+  sudo curl \
+    -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+mkdir -p /var/log
+chown -R www-data:www-data /var/log
+
+sudo \
+  WORKER_MANAGER_VERSION="$WORKER_MANAGER_VERSION" \
+  DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN" \
+  docker-compose up -d
+sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
+sudo docker-compose exec -T app php bin/console messenger:setup-transports
+sudo docker-compose exec -T app php bin/console doctrine:schema:update --force
