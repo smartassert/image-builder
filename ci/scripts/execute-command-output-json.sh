933af0998d587de0aa6161a9721ca83748d8109a
Jon Cram <webignition@gmail.com>
2021-09-07 10:48:24 +0100

    create execute-command-output-json test

--- ci/scripts/execute-command-output-json.sh
+++ ci/scripts/execute-command-output-json.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
-JSON_OUTPUT=$($COMMAND)
-EXIT_CODE=$?
+EXIT_CODE_COMMAND_ERROR=2
+EXIT_CODE_JQ_ERROR=3
 
-echo $JSON_OUTPUT | jq "."
-exit $EXIT_CODE
+COMMAND_OUTPUT=$($COMMAND)
+EXIT_CODE="$?"
+
+if [ "0" != "$EXIT_CODE" ]; then
+  echo "{
+  \"error\": \"command failure\",
+  \"command-exit-code\": $EXIT_CODE,
+  \"command-output\": \"$COMMAND_OUTPUT\"
+}"
+
+  exit "$EXIT_CODE_COMMAND_ERROR"
+fi
+
+JQ_STD_ERR="/tmp/jq.log"
+echo $COMMAND_OUTPUT | jq "." 2>"$JQ_STD_ERR"
+JQ_EXIT_CODE="$?"
+if [ "0" != "$JQ_EXIT_CODE" ]; then
+  echo "{
+  \"error\": \"jq failure\",
+  \"jq-exit-code\": $JQ_EXIT_CODE,
+  \"jq-output\": \"$(<"$JQ_STD_ERR")\"
+}"
+
+  exit "$EXIT_CODE_JQ_ERROR"
+fi
