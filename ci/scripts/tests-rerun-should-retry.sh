cd7cb2f4a71233c552790b569b5bdeebd236a9cc
Jon Cram <webignition@gmail.com>
2021-09-09 16:36:54 +0100

    Simplify should-retry logic

--- ci/scripts/tests-rerun-should-retry.sh
+++ ci/scripts/tests-rerun-should-retry.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$DURATION" ]; then
-  echo "false"
-  exit
-fi
-
-if [ -z "$MAXIMUM_DURATION" ]; then
-  echo "false"
-  exit
-fi
-
-[ $DURATION -lt $MAXIMUM_DURATION ] || [ $DURATION = $MAXIMUM_DURATION ] && echo "true" || echo "false"
+{ [ -n "$DURATION" ] && [ -n "$MAXIMUM_DURATION" ]; } &&
+{ [ $DURATION -lt $MAXIMUM_DURATION ] || [ $DURATION = $MAXIMUM_DURATION ]; } &&
+echo "true" || echo "false"
