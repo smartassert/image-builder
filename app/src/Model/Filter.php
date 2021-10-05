da0a2d65f5790466f56534f46b8265108c186eda
Jon Cram <webignition@gmail.com>
2021-10-05 15:52:05 +0100

    Define filter string format, create parser

--- app/src/Model/Filter.php
+++ app/src/Model/Filter.php
@@ -6,6 +6,11 @@ class Filter
 {
     public const OPERATOR_EQUALS = '=';
     public const OPERATOR_NOT_EQUALS = '!=';
+    public const OPERATOR_DEFAULT = self::OPERATOR_EQUALS;
+
+    private const KEY_FIELD = 'field';
+    private const KEY_OPERATOR = 'operator';
+    private const KEY_VALUE = 'value';
 
     /**
      * @param Filter::OPERATOR_* $operator
@@ -13,7 +18,7 @@ class Filter
     public function __construct(
         private string $field,
         private string $operator,
-        private bool|int|string|float $value,
+        private bool | int | string | float $value,
     ) {
     }
 
@@ -27,8 +32,26 @@ class Filter
         return $this->operator;
     }
 
-    public function getValue(): bool|int|string|float
+    public function getValue(): bool | int | string | float
     {
         return $this->value;
+    }
+
+    /**
+     * @param array<mixed> $data
+     */
+    public static function fromArray(array $data): ?self
+    {
+        $field = $data[self::KEY_FIELD] ?? null;
+        $operator = $data[self::KEY_OPERATOR] ?? self::OPERATOR_DEFAULT;
+        $value = $data[self::KEY_VALUE] ?? null;
+
+        $fieldIsValid = is_string($field);
+        $operatorIsValid = self::OPERATOR_EQUALS === $operator || self::OPERATOR_NOT_EQUALS === $operator;
+        $valueIsValid = is_bool($value) || is_int($value) || is_string($value) || is_float($value);
+
+        return $fieldIsValid && $operatorIsValid && $valueIsValid
+            ? new Filter($field, $operator, $value)
+            : null;
     }
 }
