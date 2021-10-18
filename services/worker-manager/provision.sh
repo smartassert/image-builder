353105fcc444cedf296c28520603c9ef2880aa57
Jon Cram <webignition@gmail.com>
2021-10-18 17:46:11 +0100

    Move build assets

--- services/worker-manager/provision.sh
+++ services/worker-manager/provision.sh
@@ -44,7 +44,7 @@ mkdir -p /var/log
 chown -R www-data:www-data /var/log
 
 sudo \
-  WORKER_MANAGER_VERSION="$WORKER_MANAGER_VERSION" \
+  VERSION="$VERSION" \
   DIGITALOCEAN_ACCESS_TOKEN="$DIGITALOCEAN_ACCESS_TOKEN" \
   docker-compose up -d
 sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
