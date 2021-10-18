7c8d34297593b88eb058877db6873ca44e1135f9
Jon Cram <webignition@gmail.com>
2021-10-18 15:33:27 +0100

    Add script to check service configuration exists

--- ci/scripts/check-service-configuration-exists.sh
+++ ci/scripts/check-service-configuration-exists.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-if [ ! -f "$ENV_FILE_PATH" ]; then
-  echo "Configuration for service $SERVICE_ID not found: $ENV_FILE_PATH"
+SERVICE_ID="$1"
+CONFIGURATION_PATH="$2"
+
+if [ "" = "$SERVICE_ID" ]; then
+  echo "Argument 1 (service id) not given"
   exit 1
 fi
 
-echo "$(<"$ENV_FILE_PATH")"
+if [ "" = "$CONFIGURATION_PATH" ]; then
+  echo "Argument 2 (configuration path) not given"
+  exit 2
+fi
+
+if [ ! -f "$CONFIGURATION_PATH" ]; then
+  echo "Configuration for service $SERVICE_ID not found: $CONFIGURATION_PATH"
+  exit 3
+fi
+
+echo "$(<"$CONFIGURATION_PATH")"
