397eaccc9549856f6a6f6b8a7a150ec8c3438b38
Jon Cram <webignition@gmail.com>
2021-10-20 18:38:49 +0100

    Delint

--- ci/scripts/create-gha-outputs-from-json-scalar-object.sh
+++ ci/scripts/create-gha-outputs-from-json-scalar-object.sh
@@ -2,7 +2,8 @@
 
 INPUT=$(cat)
 SET_OUTPUT_LINES=$(jq -r '. | to_entries | .[] | "::set-output name=" + .key + "::" + .value' 2>/dev/null <<< "$INPUT")
-[[ "$?" != "0" ]] && exit 1
+JQ_EXIT_CODE="$?"
+[[ "$JQ_EXIT_CODE" != "0" ]] && exit 1
 
 readarray -t SET_OUTPUT_LINES_ARRAY <<< "$SET_OUTPUT_LINES"
 for SET_OUTOUT_LINE in "${SET_OUTPUT_LINES_ARRAY[@]}"
