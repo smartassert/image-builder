e63098f386900ef283277c4f27f571098166fc0e
Jon Cram <webignition@gmail.com>
2021-11-09 18:22:00 +0000

    Verify Caddyfile existence before trying to fmt/validate

--- ci/validate-caddyfile.sh
+++ ci/validate-caddyfile.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ ! -f "$CADDYFILE_PATH" ]; then
+  echo "Caddyfile at path \"$CADDYFILE_PATH\" does not exist"
+  exit 1
+fi
+
 docker run \
   -e DOMAIN="example.com" \
   -v "$CADDYFILE_PATH" \
--- ci/verify-caddyfile-is-correctly-formatted.sh
+++ ci/verify-caddyfile-is-correctly-formatted.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ ! -f "$CADDYFILE_PATH" ]; then
+  echo "Caddyfile at path \"$CADDYFILE_PATH\" does not exist"
+  exit 1
+fi
+
 ORIGINAL=$(< "$CADDYFILE_PATH")
 FORMATTED=$(docker run -v "$CADDYFILE_PATH:/etc/caddy/Caddyfile" caddy caddy fmt /etc/caddy/Caddyfile)
 
