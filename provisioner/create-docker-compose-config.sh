aba3f08ee8376019fd83d2634748e25867806f36
Jon Cram <webignition@gmail.com>
2021-11-10 17:55:14 +0000

    (debug) worker-manager config creation 001

--- provisioner/create-docker-compose-config.sh
+++ provisioner/create-docker-compose-config.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 COMPOSE_FILES=$(ls ./docker-compose-config-source/*.yml)
+
+echo "$COMPOSE_FILES"
+
 COMMAND="docker-compose"
 
 for FILE in $COMPOSE_FILES; do
@@ -9,4 +12,5 @@ done
 
 COMMAND="$COMMAND config --no-interpolate > docker-compose.yml"
 
+echo "$COMMAND"
 eval "$COMMAND"
