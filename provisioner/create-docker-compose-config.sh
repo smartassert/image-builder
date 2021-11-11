893fe0d57766620f871e55e78992914ce2069a3d
Jon Cram <webignition@gmail.com>
2021-11-11 09:53:54 +0000

    Tidy up

--- provisioner/create-docker-compose-config.sh
+++ provisioner/create-docker-compose-config.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 COMPOSE_FILES=$(ls ./docker-compose-config-source/*.yml)
-
-echo "$COMPOSE_FILES"
-
 COMMAND="docker-compose"
 
 for FILE in $COMPOSE_FILES; do
@@ -12,5 +9,4 @@ done
 
 COMMAND="$COMMAND config --no-interpolate > docker-compose.yml"
 
-echo "$COMMAND"
 eval "$COMMAND"
