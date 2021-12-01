f25d9f0e95b12c60c49a55db26fda2e39935ce77
Jon Cram <webignition@gmail.com>
2021-12-01 14:56:29 +0000

    Pass public IP to caddy, via docker-compose, when starting

--- services/users/provision.sh
+++ services/users/provision.sh
@@ -3,4 +3,5 @@
 mkdir -p /var/log
 chown -R www-data:www-data /var/log
 
-sudo VERSION="$VERSION" docker-compose up -d
+PUBLIC_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+sudo VERSION="$VERSION" CADDY_IP="$PUBLIC_IP" docker-compose up -d
