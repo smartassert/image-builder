ad0bced3c36a39b9b387f85b86893ca06f49af00
Jon Cram <webignition@gmail.com>
2021-12-01 16:33:38 +0000

    Provider public IP to caddy, via docker-compose, on first boot

--- services/users/first-boot.sh
+++ services/users/first-boot.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+PUBLIC_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+sudo VERSION="$VERSION" CADDY_IP="$PUBLIC_IP" docker-compose up -d
+
 sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
 sudo docker-compose exec -T app php bin/console doctrine:migrations:migrate --no-interaction
