31f249e3f702398b9b18f694dc3449406ca09c6d
Jon Cram <webignition@gmail.com>
2021-12-01 15:42:43 +0000

    Output discovered public IP when provisioning

--- services/users/provision.sh
+++ services/users/provision.sh
@@ -4,4 +4,7 @@ mkdir -p /var/log
 chown -R www-data:www-data /var/log
 
 PUBLIC_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
+echo "Public IP:"
+echo "$PUBLIC_IP"
+
 sudo VERSION="$VERSION" CADDY_IP="$PUBLIC_IP" docker-compose up -d
