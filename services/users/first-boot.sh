55d31154dad485745b398d1a5436b07479fa3f19
Jon Cram <webignition@gmail.com>
2021-12-06 18:04:08 +0000

    Users service first-boot script writes jwt private key from multiple env vars

--- services/users/first-boot.sh
+++ services/users/first-boot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-base64 -d <<< "$JWT_SECRET_KEY_BASE64" > /root/jwt/private.pem
+base64 -d <<< "$JWT_SECRET_KEY_BASE64_PART1$JWT_SECRET_KEY_BASE64_PART2$JWT_SECRET_KEY_BASE64_PART3" > /root/jwt/private.pem
 base64 -d <<< "$JWT_PUBLIC_KEY_BASE64" > /root/jwt/public.pem
 
 PUBLIC_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
