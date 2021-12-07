f8fd712a1be08fbd0fc2bff745ea615d96a01f8b
Jon Cram <webignition@gmail.com>
2021-12-07 12:28:48 +0000

    Pass users JWT_PASSPHRASE to app container, persist to /etc/environment (#596)

--- services/users/first-boot.sh
+++ services/users/first-boot.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+echo "JWT_PASSPHRASE=$JWT_PASSPHRASE" >> /etc/environment
+
 base64 -d <<< "$JWT_SECRET_KEY_BASE64_PART1$JWT_SECRET_KEY_BASE64_PART2$JWT_SECRET_KEY_BASE64_PART3" > /root/jwt/private.pem
 base64 -d <<< "$JWT_PUBLIC_KEY_BASE64" > /root/jwt/public.pem
 
 PUBLIC_IP=$(dig @resolver4.opendns.com myip.opendns.com +short)
-sudo VERSION="$VERSION" CADDY_IP="$PUBLIC_IP" docker-compose up -d
+sudo \
+  VERSION="$VERSION" \
+  CADDY_IP="$PUBLIC_IP" \
+  JWT_PASSPHRASE="$JWT_PASSPHRASE" \
+  docker-compose up -d
 
 sudo docker-compose exec -T app php bin/console doctrine:database:create --if-not-exists
 sudo docker-compose exec -T app php bin/console doctrine:migrations:migrate --no-interaction
