289118c775806d3658a86161d32fcb7478ef95ff
Jon Cram <webignition@gmail.com>
2021-12-09 14:58:22 +0000

    Revert to previous script with sorted lisets

--- provisioner/list-non-running-docker-compose-services.sh
+++ provisioner/list-non-running-docker-compose-services.sh
@@ -1,63 +1,8 @@
 #!/usr/bin/env bash
 
-STOPPED_SERVICES="$(docker-compose ps --services --filter "status=stopped")"
-PAUSED_SERVICES="$(docker-compose ps --services --filter "status=paused")"
-RESTARTING_SERVICES="$(docker-compose ps --services --filter "status=restarting")"
-
-NON_RUNNING_SERVICES=""
-if [ "" != "$STOPPED_SERVICES" ]; then
-  NON_RUNNING_SERVICES="$STOPPED_SERVICES"
-fi
-
-if [ "" != "$PAUSED_SERVICES" ]; then
-  if [ "" != "$NON_RUNNING_SERVICES" ]; then
-    NON_RUNNING_SERVICES="$NON_RUNNING_SERVICES
-"
-  fi
-
-  NON_RUNNING_SERVICES="$NON_RUNNING_SERVICES$PAUSED_SERVICES"
-fi
-
-if [ "" != "$RESTARTING_SERVICES" ]; then
-  if [ "" != "$NON_RUNNING_SERVICES" ]; then
-    NON_RUNNING_SERVICES="$NON_RUNNING_SERVICES
-"
-  fi
-
-  NON_RUNNING_SERVICES="$NON_RUNNING_SERVICES$RESTARTING_SERVICES"
-fi
-
-ALL_SERVICES=$(docker-compose ps --services | sort)
-NON_RUNNING_SERVICES=$(sort <<< "$NON_RUNNING_SERVICES")
-
-RUNNING_SERVICES=$(comm -13 <(echo "$NON_RUNNING_SERVICES") <(echo "$ALL_SERVICES")>)
-
-echo "All services:"
-echo "$ALL_SERVICES"
-echo "Running services:"
-echo "$RUNNING_SERVICES"
-echo "Stopped services:"
-echo "$STOPPED_SERVICES"
-echo "Paused services:"
-echo "$PAUSED_SERVICES"
-echo "Restarting services:"
-echo "$RESTARTING_SERVICES"
-
+RUNNING_SERVICES="$(docker-compose ps --services --filter "status=running" | sort)"
+ALL_SERVICES="$(docker-compose ps --services | sort)"
 if [ "$RUNNING_SERVICES" != "$ALL_SERVICES" ]; then
-    docker-compose ps
-
-    echo "All services:"
-    echo "$ALL_SERVICES"
-    echo "Running services:"
-    echo "$RUNNING_SERVICES"
-    echo "Stopped services:"
-    echo "$STOPPED_SERVICES"
-    echo "Paused services:"
-    echo "$PAUSED_SERVICES"
-    echo "Restarting services:"
-    echo "$RESTARTING_SERVICES"
-
-    docker-compose logs
-
+    comm -13 <(sort <<<"$RUNNING_SERVICES") <(sort <<<"$ALL_SERVICES")
     exit 1
 fi
