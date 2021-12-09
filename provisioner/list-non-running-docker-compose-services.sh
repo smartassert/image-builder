c7505240e1a432f5fe9218e15a25bebc33668331
Jon Cram <webignition@gmail.com>
2021-12-09 16:04:20 +0000

    Re-implement checking if docker-compose services are running (#618)

--- provisioner/list-non-running-docker-compose-services.sh
+++ provisioner/list-non-running-docker-compose-services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-RUNNING_SERVICES="$(docker-compose ps --services --filter "status=running")"
-ALL_SERVICES="$(docker-compose ps --services)"
+RUNNING_SERVICES="$(docker-compose ps --services --filter "status=running" | sort)"
+ALL_SERVICES="$(docker-compose ps --services | sort)"
 if [ "$RUNNING_SERVICES" != "$ALL_SERVICES" ]; then
     comm -13 <(sort <<<"$RUNNING_SERVICES") <(sort <<<"$ALL_SERVICES")
     exit 1
